f824159bbc27de92eb542c6328310165e1aa1997
Stella <aispacevir4812@gmail.com>
2022-06-08 20:52:05 +0300

    suomi n norsk

---
+++ norsk.py
@@ -0,0 +1,14 @@
+'''
+hei og velkomimen til en my ny cpe det er mange folk som liker å
+jogge langs der er en mann som jagger jeg er ute og jogger
+målet mitter a jogge 10km i dag jeg er na halvveis og har
+lopt 5km vi sees har 5 na er jeg ferdig med a løpe og jeg løp
+lopå 1 time og 10 minutter så nå skal det bli deilig a spise
+litt mat og slappe au nå har jea trent nok for i dag i byen
+hor de fleste i leiligheter i blokker dette er en blokk en blokk
+er en bygning med mange etasjer og mange leiligheter en kjegle
+står på gresset jeg skal gå over veien og det kan jeg gjøre siden
+det er grønt lys na er det rødt lys pa trærne kan vi se mange
+grønne blader denne brua gar over akerselua her kan dere se
+akosenselua jeg gar på fortanet
+'''
