0b4725b0863c79e28acf6ea9b57245f7a57b503f
stella-vir <stellavir11@gmail.com>
2022-10-28 21:08:29 +0300

    uhmakkaasti sinnikkäästi

---
+++ meijeri.py
@@ -0,0 +1,36 @@
+'''
+# lokakuu kaks kyt kaheksan
+
+Se kuulostaa minusta tylsältä työltä, seuraa sitten sydäntäsi.
+se voi tuntua jokus nyt vaikealta, mutta se tarkoittaa myös,
+että opit, älä pyydä anteeksi yrittämistäsi
+
+oon hyvä työntekijä, koska
+oon täsmällinen-precise ja oma-aloitteinen-proactive
+
+se on mun vikani, mutta mitä voin tehä asialle
+se on vaikeaa, olin täysin idiootti
+
+asettaa korkeampi tavoite
+keskity vain siihen, mitä voit tehä,
+mitä enemmän teet,
+sitä parempi tulos
+
+aina ku joku jättää sinut,
+sitä enemmän haluat jäädä
+
+joskus jäät jumissa menneisyyteen
+
+uhmakkaasti -defiantly
+'''
+
+
+
+
+
+
+
+
+
+
+# end
---
+++ numero.py
@@ -0,0 +1,12 @@
+'''
+Kaheksasta neljään
+yheksästä viiteen
+kahentoista minuutin
+yhentoista
+kolkytseittemän
+
+helm 2 heina 7
+loka 10 marr 11
+ham 1
+touko 5
+'''
