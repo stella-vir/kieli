c45eef265f8941fee98346948920e70326b4595a
stella-vir <stellavir11@gmail.com>
2022-10-29 19:29:14 +0300

    täytyy pysty pysya pyytää pystyin pystiskö

--- meijeri.py
+++ meijeri.py
@@ -11,13 +11,20 @@ oon täsmällinen ja oma-aloitteinen -precise -proactive
 se on vaikeaa, olin täysin idiootti
 se on mun vikani, mutta mitä voin tehä asialle
 
-asettaa korkeampi tavoite -set
+asettaa korkeampi tavoite -set higher
+korkeintaan -max.
 keskity vain siihen, mitä voit tehä,
 mitä enemmän teet,
 sitä parempi tulos
 
+mä saan jättää tän -leave
+en viittiny laatikkoon jättää -leave in the mailobx
 aina ku joku jättää sinut, -leave
 sitä enemmän haluat jäädä  -stay
+en oo jäämässä suomeen ja lopettamassa
+mä jaan nää murteet maantieteellisesti -divide and conquer -dialects
+jaetaan -share
+kun se jakaa postia -share-deliver the mail
 
 joskus jäät jumissa menneisyyteen -stuck
 
@@ -37,7 +44,142 @@ tai voit tehoä asialle jotain
 
 ansaitset sen, sekä huonolla että hyvällä tavalla -deserve
 teit valinnan, se oli aina sinä -choice
+
+can
+jos pystyy keskittyä -can concentrate
+on pystyä käydä kaupassa -can go shopping
+en tiiä pysytiskö käydä enää
+mä pystyin -could
+
+yrittäkää pysyä mukana -try to stay along
+pitäiskö pyytää se kylään -ask
+
+saa ostettua ruokakaupasta -can be bought
+
+kätevää -handy
+
+
+pohjänmaalta
+mä mie
+sä sie
+mennään menhän
+taloon talhon
+
+Etelä-pohjänmaalta
+me taidetaan saada sadetta
+me tairetahan saara saretta
+tulee tuloo
+menee menöö
+
+yritän välttää niitä -avoid
+joutua -end up
+kiertää sit naapurustossa ovelta ovelle -around
+pitkää ikää -age
+yllätys pelotella -scare
+se pelasti äidin -save
+ettei loiskuta vettä joka puolelle -splash
+loskaisen kelin -slushy weather
+täällä sais olla kumpparit jalassa tällasella säällä -wellies on the feet
+käyt päälle vasaran kanssa -hammer
+
+ei sais kantaa edes perille -carry there
+laitonta illegal
+olen erittäin pahoillani
+verikokeeseen blood test
+
+yritän etsiä -find
+totetuttavat unelmansa -dream come true
+kyseenalaistaa question
+ruuhkassa traffic jam
+
+keräilen collect
+
+sä oot iloinen mitä hän lähettää, joten -so
+
+uljas ratsu siivouskärryni -proud-gallant knight-steed cart
+tienaan vähan rahaa -earn
+
+elää hetkessä
+
+omaisuuteni property
+rannatoomuus landlessness
+
+miten ihmiset heräilee siihen ajatukseen -wake up to
+
+pyörittää meidän perheyritystä -run
+kuskata ne mun äidin kellariin -take
+varjopuolensa shadow
+luonnonvoimat -natural force
+arkea everyday
+harvemmin rarely
+harvoin kiroilen, mut on kyllä paska päivä -swear
+maanjäristykseen earthquake
+niin tuntuu siltä, että se kestäisi ikuisuuden -last forever
+kerkiää myös miettii emerge
+katto rupee romahtamaan -the roof starts to fall off
+välillä between
+kun ehtii tottuu -get used to it
+mihin on tottunu -what is it used to
+pysyvästi permanently
+
+haastavaa -challenges
+jotenkin aliarvioi itseään -underestimate
+epäröi -hesitate
+
+asettu aloillen -settle down
+
+haaveilee -dream of it
+puristaa appelsiinimehunsa -squeeze
+noin sitä itekin oli ennen -like it was before
+palo tai valo
+mä oon tajunnu -realize
+ahdistava -oppressive
+se on yks terapian muoto -form of therapy
+pakokeino -getaway a way to escape
+
+urhea -brave
+terveydeksi -bless you
+
+nousetko seisomaan se kädessäsi
+meidät kutsutaan tapahtumiin  -invited to events
+vakavasti -seriously
+yhteiset päivälliset -dinner
+iski viimeistään -stuck at the latest
+käydaan laudat -(surf) board
+rupeaisi uutta lautaa -start new board
+tuntumaan -seems
+käry tästä hommasta -get rid of sth-business
+vihdoin tehtaalla -finally factory
+
+se luetaan niin kuin se on kirjoitettu -read
+
+saarelle island
+vietetään tulevaisuudessakin -celebrated
+tulevaisuuden kannalta -in terms of
+lähdössä hakemaan avaimia -left-departure get
+ylipäätään -at all
+
+tiensä kotiin -way home
+
+suunnittelisi -plan
+ei turhaan sanota -needless to say
+puoleensavetävää attractive
+on täällä jotain, miks on niin hyvä olla -there's sth
+erityinen ihminen mun luokse -to me
+
+saman katon alle asustelemaan
+
+odotan sitä innolla -look forward to it
+pääsee jakamaan hyvät ja huonot hetket
+häpeilemättä shamelessly
+hihhuloiden snort
+parisuhde relationship
+
+se tulee sitten kun tulee
+ja jos ei tuu, kyl se tulee -it will come
+
 '''
+
 
 
 
---
+++ työ.py
@@ -0,0 +1,60 @@
+'''
+mä tiiän että monet teistä asuu jo Suomessa
+ja ehkä ettii töitä Suomesta
+ja vaikka nykyään monessa työpaikassa
+ei vaadita suomen kielen osaamista
+niin on kuitenkin hyvä pystyä käydä
+työhaastattelussa suomeks
+netistä löytyy tosi paljon vinkkejä
+miten selviytyä hyvin työhaastatteluista
+joten mä en keskity tässä videossa siihen
+vaan mä vastaan kolmeen yleiseen kysymykseen
+mitä yleensä työhaastatteluissa kysytään
+sillä tavalla saatte vähän vinkkejä ja ideoita
+miten vastata näihin kysymyksiin suomeks
+
+miksi haet tätä työpaikkaa
+musta tuntuu, että mulla on paljon annettavaa
+ja uusia ideoita
+ja että tää työpaikka on niinku mulle tehty
+koska mä oon aina ollu kiinostunu tässtä aiheesta
+tää työpaikka olis tosi hyvä mahdollisuus mulle
+edetä mun uralla ja kehittää mua
+työntekijänä ja ihmisenä
+mun unelmana on tehdä jotain, mikä on mulle tärkeetä
+ja mä uskon, että se on ois mahdollista tässä työpaikassa
+
+mitkä on sun hyvät ja huonot puolet
+mun hyvät puolet on, että mä oon tosi tunnollinen
+ja teen aina parhaani kaikessa, mihin ryhdyn
+ja mä oon myös tosi järjestelmällinen
+ja mulla on korkee stressinsietokyky
+mun heikkous on etta mä oon ehkä liian kiltti
+enkä osaa sanoa ei jos mulle annetaan lisätehtäviä
+mikä saattaa johtaa sit ylitöihin sillon tällön -result in -occasionally
+
+missä näet itsesi viiden vuoden kuluttua
+italiassa rannalla makoilemassa
+ei vaiskaan
+mä haluan kehittyä mun uralla
+ja tällä alalla
+joten viiden vuoden kuluttua
+mä nään itteni uuden kehitysyksikön -development unit
+apulaispäällikönä - deputy-assistant manager
+
+sait työpaikan!
+
+tassä oli ssis kolme yleistä
+työhaastattelukysymystä
+ja esimerkkivastauksia
+joita voitte sit
+muokata oman tilanteene mukaan
+tsemppiä työhaastatteluun
+
+
+
+
+
+
+
+'''
