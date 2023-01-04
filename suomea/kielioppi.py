a89cc832bc562a32d18a8c46cee8f0e098dbd201
Stella <stellavir11@gmail.com>
2023-01-04 23:19:21 +0200

    Add files via upload

---
+++ suomea/kielioppi.py
@@ -0,0 +1,15 @@
+kauppojen aukioloaikoihin-olla suunitellaan laajennuksia-extends
+niiden toivotaan selkeyttävän-clear up sekavia-confusing sääntöjä
+esityksen-presentation mukaan-according to
+kaikki vähittäiskaupat-retail stores
+saisivat-would be allowed olla auki sunnuntaisin
+toukokuusta vuoden loppuun
+samalla sunnuntain aukiolo lyhenisi tunnilla-with
+iltayhdeksän-9pm sijasta-instead of oveet pitäisi panna kiinni
+jo kahdeksalta
+selonteon-report pohjana-the basis of
+on kaupan työryhmän mietintö-brainstorm
+jossa esitettiin kompromissia,
+jossa ääripäät-extremes on karsittu-prune.
+Tavoitteena-goal on selkeyttä aukioloaikoja
+jotka ovat tätä nykyä hyvin sekavia.
