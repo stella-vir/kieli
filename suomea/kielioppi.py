3d5dc32988e45299ece1fb02ee8cc3f8bb779f79
Stella <stellavir11@gmail.com>
2023-01-06 01:06:43 +0200

    Update kielioppi.py

--- suomea/kielioppi.py
+++ suomea/kielioppi.py
@@ -13,3 +13,43 @@ jossa esitettiin kompromissia,
 jossa ääripäät-extremes on karsittu-prune.
 Tavoitteena-goal on selkeyttä aukioloaikoja
 jotka ovat tätä nykyä hyvin sekavia.
+
+tulessaan
+lapsi tuo tulessaan vanhemmilleen tuhansia kysymyksiä 
+eräs tärkeä seikka-point-fact on turvallisten olojen-condition 
+järjestäminen
+järjestäminen-when organizing pienokaiselle autoilemaan 
+lähdettäessä
+lähdettäessä-when leaving 
+lapsille suunnattuja-for turvakaukaloita-safety troughs 
+ja istuimia on kyllä markkinoilla runsaasti-a lot
+mut ovatko ne laadukkaita-quality ja kunnollisia
+miten erotaa hyvän huonosta 
+tuoreille-new isille ja äideille on nyt tiedossa ajantasaista-up-to-date asiaa lasten 
+kuljettamisesta 
+kuljettamisesta-transporting autossa
+turvaistuinten tasoa on selvitty-cleared jälleen-again 
+yleiseurooppalaisessa euro test tutkimuksessa
+joka
+joka julkaistaan perinteisesti näin kesän kynnyksellä-dawn of summer-threshold
+
+arkkitehti alvar aallon suunnittelemaa 70-vuotiasta klassikkomaljakkoa 
+aletaan juhlavuoden kunniaksi valmistaa uusissa väreissä
+juhlavuoden väriksi on valittu petrolinsininen 
+
+sävyissä
+viileän sinisen sävyissä-shades saa perinteistä keskikokoista ja pieniä 
+aalto-maljakoita ja maljoja
+
+kokeili
+alvar aalto kokeili petrolinsinistä varhaisiin-early maljakkoihinsa-vases
+korkeita aaltoja löytyy nyt myös liekehtivän-flaming punaisena 
+
+saakka
+rubiininpunaisena aalto-maljakkoa puhallettii-was blown 1980-luvulta viime vuoteen saakka-until
+lisäksi aalto-vaaseja valmistuu nyt myös kaksivärisinä
+muokattu
+
+
+
+
