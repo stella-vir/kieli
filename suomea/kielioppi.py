a10d4c925d6ce6db1283256556e2107c99749a75
Stella <stellavir11@gmail.com>
2023-01-06 12:36:19 +0200

    Update kielioppi.py

--- suomea/kielioppi.py
+++ suomea/kielioppi.py
@@ -1,4 +1,6 @@
 kauppojen aukioloaikoihin-olla suunitellaan laajennuksia-extends
+
+selkeyttävän
 niiden toivotaan selkeyttävän-clear up sekavia-confusing sääntöjä
 esityksen-presentation mukaan-according to
 kaikki vähittäiskaupat-retail stores
@@ -9,6 +11,8 @@ iltayhdeksän-9pm sijasta-instead of oveet pitäisi panna kiinni
 jo kahdeksalta
 selonteon-report pohjana-the basis of
 on kaupan työryhmän mietintö-brainstorm
+
+jossa
 jossa esitettiin kompromissia,
 jossa ääripäät-extremes on karsittu-prune.
 Tavoitteena-goal on selkeyttä aukioloaikoja
@@ -17,8 +21,10 @@ jotka ovat tätä nykyä hyvin sekavia.
 tulessaan
 lapsi tuo tulessaan vanhemmilleen tuhansia kysymyksiä 
 eräs tärkeä seikka-point-fact on turvallisten olojen-condition 
+
 järjestäminen
 järjestäminen-when organizing pienokaiselle autoilemaan 
+
 lähdettäessä
 lähdettäessä-when leaving 
 lapsille suunnattuja-for turvakaukaloita-safety troughs 
@@ -26,6 +32,7 @@ ja istuimia on kyllä markkinoilla runsaasti-a lot
 mut ovatko ne laadukkaita-quality ja kunnollisia
 miten erotaa hyvän huonosta 
 tuoreille-new isille ja äideille on nyt tiedossa ajantasaista-up-to-date asiaa lasten 
+
 kuljettamisesta 
 kuljettamisesta-transporting autossa
 turvaistuinten tasoa on selvitty-cleared jälleen-again 
@@ -50,6 +57,51 @@ rubiininpunaisena aalto-maljakkoa puhallettii-was blown 1980-luvulta viime vuote
 lisäksi aalto-vaaseja valmistuu nyt myös kaksivärisinä
 muokattu
 
+tavoin
+opettajan ammatti kehittyy monilla tavoin.
+samalla kun opettajat opiskelevat erilaisten
+tietokoneohjelmien käyttöä
+heidän tulisi pysyä selvillä
+oman alansa kehityksestä 
+ja hoitaa työnsä tarpeeksi hyvin
+opetajalta vaaditaan myös 
+
+yleissivistystä-general education
+paljon yleissivistystä jotta hän voisi
+toimia sivistyksen välittäjänä-mediator 
+opiskelijoille
+kulttuurin ja yhteiskuntatiedon opettaminen
+on tärkeää riippumatta siitä
+
+toimiiko
+toimiiko opettaja peruskoulussa vai 
+ammatillisessa koulutuksessa
+
+autokoulut kokeilevat nuorille
+ajo-opetusta ryhmissä
+ne toivovat että nuoret oppivat 
+hallitsemaan hermonsa
+vaikka takapenkkiläiset meluavat-making noises
+ryhmäopetus on yksi tapa,
+jolla koetetaan vähentää 
+nuorten riskejä liikenteessä
+
+matkustajista
+nuorista auton matkustajista-passengers
+
+kolmas
+suurin osa ei käytä joka kolmas
+lähes 70 prosenttia nuorista 
+on joskus lähtenyt vieraan kuljettajan kyytiin
+autokoulussa nuorille opetetaan
+
+vaaratilanteen
+myös vaaratilanteen ennakointia-precaution
+mielikuvaharjoituksessa-imagery
+kuljettaja kuvittelee 
+
+onnettomuudessa
+itsensä ajamaan onnettomuudessa
 
 
 
