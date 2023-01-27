bd6a8a024e5731734725a6997be3088033672a9f
Stella <stellavir11@gmail.com>
2023-01-27 21:27:28 +0200

    Add files via upload

---
+++ lause.py
@@ -0,0 +1,46 @@
+mikä olla-verbi on
+mitä muut verbit puhut
+
+mikä päivä tänään on
+mitä kieltä sinä puhut
+
+vahva-k p t  heikko-  v d
+ei sk st tk
+ei 2 vokaalia lopussa vapaa-vapaassa
+
+turku turussa
+kylpy kyvyssä
+katu kadulla
+
+helsinki helsingissä
+irlanti irlannin
+kampa kammalla
+
+ystäväni
+kirjasi
+hänen autonsa
+opettajamme
+kotimaanne
+heidän kurssinsa
+
+nt-nn antaa-annan kääntää-käänan lentää-lennan
+tt-t kirjoittaa-kirjoitan ottaa-otan
+kk-k nukkua-nukun-nukkuu
+k- - lukea-luen pukea-puen
+nk-ng onkia-ongin
+t-d tietää-tiedän
+rt-rr ymmärtää-ymmärran
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end
