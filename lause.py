c03728e0baa1e15e83e4c2e632ca49989d0532f7
Stella <stellavir11@gmail.com>
2023-01-28 14:18:41 +0200

    Add files via upload

--- lause.py
+++ lause.py
@@ -31,23 +31,109 @@ nk-ng onkia-ongin
 t-d tietää-tiedän
 rt-rr ymmärtää-ymmärran
 
-partitiivi
+# partitiivi
 partitiivissa ei kpt vaihtelua
 vokaali a-ä
 taloa valoisaa kylmää
 ia-ea-eä a-ä
 italiaa vaikeaa pimeää
 2 vokaalia-konsonantti ta-tä
+10 puutä
 maata-puhelinta-kaunista-telivisiota
 e tta-ttä
 kirjettä-venettä-osoitetta-huonetta
 
-numero partitiivi
+# numero partitiivi
 9 pitkää miestä
 3 hyvää tietokonetta
 monta-vähän-nolla-pari-puoli astetta
-puhua saksaa -rakastaa -etsiä-etsii työpaikkaa -soittaa -katsoa -ymmärtää -auttaa mua
-mulla ei ole autoa 
+
+# verbi partiviivi
+puhua saksaa -rakastaa -etsiä-etsii työpaikkaa
+-soittaa -katsoa -ymmärtää -auttaa mua-häntä-meitä-teitä-heitä-niitä
+
+pelaa jalkapalloa
+opiskelen historiaa
+juhlimme Tiinan syntymäpäivää lauantaina
+voinko häiritä sua
+
+
+mulla on vanha auto
+isolla koiralla on luu(bone)
+mulla on jano
+hänellä on punainen tukka
+
+mulla ei ole autoa
+heillä ei ole isoa perhettä
+Mialla ja Pialla ei ole testiä huomenna
+mulla ei ole jano(ei partitiivia)
+
+# genetiivi -n kanssa
+pedro asuu hannan kanssa
+olga on pedron ja alexin kanssa kurssilla
+hän puhuu opettajan kanssa
+
+# kello
+kello on kahdeksan illalla
+yhdeltä kahdeksalta yhdeltätoista
+
+kysyn myyjältä
+tarkistan varastosta
+
+voinko sovittaa tätä - tämä on sopiva
+
+?????????????????
+(sivu 96)
+# nukkua kpt
+nukun
+# hymyillä
+hymyilette
+# ehtiä
+ehtii
+# laina
+lainaan
+
+# juoda
+juon
+# nähdä
+näen näette näkevät
+# myydä
+myy
+#siivota
+siivoaa
+#tulkita
+tulkitsevat
+
+# purra
+puren(bite)
+# nousta
+nousen
+
+#haluta
+haluan
+
+# häiritä
+häiritsen  häiritsee
+harkitsen(considering)
+
+hän ei syö spagettia
+me emme opiskele
+he eivät tarvitse rahaa
+
+# tavallisesti
+posti tulee tavallisesti
+
+korkea-matala
+leveä(wide)-kapea(narrow)
+viihtyisä(homey)-sotkuinen(messy)
+
+# mistä 
+kotoa ulkoa sisältä
+# missä
+kotona ulkona sisällä
+# mihin
+kotiin ulos sisään
+
 
 
 
