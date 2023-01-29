eaf26f758896c386ebb967fc8bb1c6db71a5e915
Stella <stellavir11@gmail.com>
2023-01-29 20:54:58 +0200

    Add files via upload

---
+++ tehtävä.py
@@ -0,0 +1,339 @@
+autokaupan mainos
+kaara autoalan asiantuntemusta 40 vuotta
+tänä vuonna 40 vuotta täyttäville uuden auton ostajille
+lahjaksi 4x40 litraa polttoainetta
+koeajajat osallistuvat juhlakisaan jossa voi voitttaa hemmoteluviikonlupun
+2 henkilölle city-kylpylässä tai kaara-tuotepaketteja
+
+autokaupalla on syntärijuhlavuosi
+tänä vuonna auton ostavat saavat kylpyläloman
+kaara autoalan asiantuntemusta 40 vuotta
+tänä vuonna 40 vuotta täyttäville uuden auton ostajille
+lahjaksi 4x40 litraa polttoainetta
+koejajat osallistuvat juhlakisaan
+jossa voivoittaa hemmotteluviikonlupun 2 henkilölle city-kylpylässä tai kaara-tuotepaketteja
+
+
+kalastuskilpailu
+osallisut suur keuruu lehden suureen haukikisaan ja voita soutuvene
+haukikilpailun järjestää suur keuruu yhteistyökumppaneiden kanssa
+kisaan voi ottaa osaa hauella joka on saatu laillisin kalastusvälinein ajalla
+2.6 31.8.2006 kisan kalastusalueen muodostavat seuraavien kuntien alueet
+keuruu multia mänttä vilppula ja petäjävesi
+kisaan voi osallistua niin monella hauella kuin haluaa
+hauki on punnittava keuruula miken koneessa tai keurrun sahassa ja erässä
+niiden aukioloaikana
+hauen voi punnita myös luotettavalla vaaalla
+joka on tarvittaessa kisan tuomariston tarkistettavissa
+kahden puolueettoman henkilön tulee todistaa punnitus
+kisan ykköspalkinto arvotaan kaikkien
+osallistuneiden kesken
+muut palkinnot menevät järjestyksessä
+viiden painavimman hauen saajalle
+painavimman hauen saaja saa vuodeksi haltuunsa
+suur keuruun kiertopalkinnon
+sen saa lupulta omaksi se joka voitta kisan kolme kertaa
+hauki on ilmoitettava kissan valmiiksi esitäytetyllä lomakkeella
+jonka saa suur keuruun sivulta lehden toimituksesta tai keuruun sahasta ja erästä
+sekä mikin koneesta keuruulta
+lähetä kuva saaliistasi
+suur keuruu ulkaisee tulospörssin ja kuvia kalastajista
+saaliiineen kalan koosta riippumatta
+
+haukikilpailun järjestää
+keuruu multia mänttä vilppula ja petäjävesi
+mikin kone ja keuruun saha ja erä
+suur keuruu lehti ja yhteistyökumppanit
+
+hauen voi punnita itse
+kilpailutuomarin vaa alla
+kilpailutuomarin kanssa
+todistajien kanssa
+
+pääpalkinnon saa se joka voittaa arvonnan
+kalastaa painavimman hauen
+kalastaa ensimmäisen hauen
+suur keuruun kiertopalkinnon saaa itselleen
+jos voitttaa haukikilpailun
+tänä vuonna
+suur keuruu julkaisee kuvia parhaista kalastajista
+kaikenkokoisista kaloista isoista kaloista
+
+kalastuskilpailu
+osallistu suur keuruu lehden suureen haukikisaan ja voita suotuvene
+haukikilpailun järjestää suur keuruu yhteistyökumppaneiden kanssa
+kissaan voi ottaa osaa haueella joka on saatu lailli
+haukikilpailunjärjestää suur keuruu yhteistyökumppaneiden kanssa
+kisaan voi ottaa osaa heuella joka on saatu laillisin kalastus välinein ajalla 2.6
+kisan kalastusalueen muodostavat seuraavien kuntien alueet keuruu multia måäntää vilppula ja peräjävesi
+kissan voi osallistua niin monella haurlla kuin haluaa hauki on punnittava keuruula mikin koneessa
+tai keuruun sahassa ja erässä niiden aukioloaikana
+hauen voi punnita myös luotettavalla vaa alla
+joka ontarvittaessa kisan tuomariston tarkistettavissa
+kahden puolueeetttoman henkilön tulee todistaa punnitus
+kisan ykköspalkinto arvotaan kaikkien osallistuneiden kesken
+muut palkinnot menevät järjestykesessä viiden painavimman hauen saajalle
+painavimman hauen assaja saa vuodeksi haltuunsa suur kwurrun kiertopalkinnonsen
+sen saa lopulta omaksi se joka voittta kisan kolme kertaa
+hauki on ilmoitettava kissan valmiiksi esitäytetyllä lomakkeella
+jonka saa suur keuruun sivulta
+lehden toimituksesta tai keuruun sahasta ja erästä sekä mikin koneesta keuruulta
+lähetä kuva saaliistasi suur keuruu julkaisee tulospörssin ja kuvia kalastajista saaliineen kalan koosta riippumatta
+
+haukikilpailun järjestää
+keuruu multia mänttä vilppula ja petäjävesi
+mikin kone ja keuruun saha ja erä
+suur keuruu lehti ja yhteistyökumppanit
+
+hauen voi punnita itse
+kilpailutuomarin vaa alla
+kilpailutuomarin kanssa
+todistajien kanssa
+pääpalkinnon saa se joka voitta arvonnan kalastaa painavimman hauen
+kalastaa ensimmäisen hauen
+kalastaa painavimman hauen
+kalastaa ensimäisen hauen
+suur keuruun kiertopalkinnon saa itselleen jos voittaa haukikilpailun tnän vuonna kaksi kertaa kolme kertaa
+ suur keuruu julkaisee kuvia parhaista kalastajista
+ kaikenkokoisista kaloista
+ isoista kaloista  isoista kaloista kaikenkokoisista kaloista
+ parhaista kalastajista kaikenkokoisista kaloista
+
+kalastuskilpailu
+osallistu suur keuruu lehden suureen haukikisan ja voita suotuvene
+haukikilpailun järjestää suur keuruu yhteistyökumppaneiden kanssa
+kisaan voi ottaa osaa hauella joka on saatu laillisin kalastusvälinein
+ajalla
+kisan kalastusalueen muodostavat seuraavien kuntien alueet
+kisaan voi osallistua niin monella hauella kuin haluaa
+kisaan voi ottaa osaa hauella joka on saatu laillisin kalastusvällinein ajalla
+kisan kalastusalueen muodostavat seuraavien kuntien alueet
+kissan voi osallistua niin monella hauella kuin haluaa
+osallistua niin monela hauella kuin haluaa
+hauki on punnittava keuruulla mikin koneessa tai keuruun sahassa
+kaissan voi osallistua niin monella hauella kuin haluaa
+hauki on punnittava keuruulla mikin koneessa tai keuruun sahassa
+ja erässä niiden aukiolosikana
+hauen voi punnita myös luotettqavalla vaa alla joka on
+hauen voi punnita myös luotettavalla vaa-alla
+joka on tarvittaessa kisan tuomariston tarkistettavissa
+kahden puolueettoman henkilön tulee todistaa punnitus
+kisan ykköspalkinto arvotaan kaikkien osallistuneiden kesken
+kisan ykköspalkinto arvotaan kaikkien osallistuneiden kesken
+kisan ykköspalkinto arvotaan kaikkien osallistuneiden kesken
+muut palkinnot menevät järjestyksessä viiden painavimman hauen saajalle
+painavimman hauen saaja saa vuodeksi haltuunsa
+muut palkinnot menevät järjestyksessä viiden painavimman hauen saajalle
+painavimman hauen saaja saa vuodeksi haltuunsa suur keuruun kiertopalkinnon
+sen saa lopoulta suur keuruun kiertopalkinn
+sen saa lopulta omaksi se joka voittaa kisan kolme kertaa
+hauki on ilmoitettava kisaan valmiiksi esitäytetyllä lomakkeella
+jonka saa suur keuruun sivulta
+hauki on ilmoitettava kisaan valmiiksi esitäytetyllä lomakkeella
+jonka saa suur keuruun sivulta
+hauki on ilmoitetta kisaan valmiiksi esitäytetykkä lomakkeella
+jonka saa suur keuruun sivulta
+lehden toimituksesta tai keuruun sahasta ja erästä sekä mikin koeesta keuruulta
+lehden toimituksesta tai keuruun sahasta ja erästä sekä mikin koneesta keuruulta
+lehden toimituksesta tai keuruun sahasta ja erästä sekä mikin
+lehden toimitusesta tai keuruun sahasta ja erästä sekä mikin koeesta keuruulta
+läherä kuva saaliistasi
+suur keuruu ulkaisee tulospörsssin ja kuvia kalstajista saaliinen kalan koosta riipumatta
+suur keuruu julkaisee tulospörsiin ja kuvia kalastajista saaliineen kalan koosta riipumatta
+saaliineen kalan koosta riipumatta
+
+haukikilpailun järjestää keuruu multia mäntää vilppula
+suur keuruu lehti ja yhteistyökumppanit
+hauen voi punnita itse todistajien kanssa
+pääpalkinnon saa se joka voittaa arvonnan
+suur keuruun kiertopalkinnon saa itselleen jos voittaa haukikilpailun kolme kertaa
+suur keuruu julkaisee kuvia kaikenkokoisista kaloista
+
+
+valokuvakilpailu
+description ekemisen meininki valukuvakilpailuun toivotaan ennen julkaisemattomia kuvia ihmisistä työn
+toivotaan ennen julkaisemattomia kuvia ihmisistä työn touhussa suomalisella maaseudulla
+touhussa suomalaisella maaseudulla
+tekeminen voi olla yhtä hyvin elannon eteen tehtävää työtä kuin harrastustakin'
+ennen julkaisemattomia kuvia ihmisistä työn touhussa suomalaisella maaseudulla
+tekeminen voi olla yhtä hyvin elannon eteen tehtävää työtä kuin harrastustakin
+pääasia että kuvassa näkyy kunnon meininki
+kilpailu kestää syyskuun loppuun asti
+juhlavuoden kuvakilpailun voittaneen kuvan ottaja palkitaan 500 euron arvoisella valokuvausliikkeen
+lahjakortilla
+tekeminen voi olla yhtä hyvin elnnon eteen tehtävää työtä kuin harrastustakin
+pääasia että kuvassa näkyy kunnon meininki
+kilpailu kestää syyskuun loppuun asti
+kilpailu kestää syyskuun lopppun asti
+kilpailu kestää syyskuun loppuun asti
+juhlavuoden kuvakilpailun voittanen kuvan ottaja palkitaan 500 euron
+arvoisella valokuvausliikkeen lahjakortilla
+kuvia kannattaa lähettää pitkin kesää sillä jokaisen kilpailuviikon paras
+kuva julkaistaa perjantain lehdessä ja sen ottaja palkitaan kirjapalkinnolla
+kilpailukuvia voi lähettää sähköisesti jpg muodoss sähköpostiosoitteeseen'
+photo@maaseudentulevaisuus.fi.
+kuvatiedoston koko saa olla korkeintaan 5 mt
+paperikuvat postitetaan lehden toimitukseen
+kumpaankin tunnus tekemisen meininki
+mikäli haluat paperikuvat takaisin liitä mukaan postimerkillä ja
+palautusosoitteella varurstettu kirjekuori
+muista liittää mukaan myös yhteysteitosi puhelinnumeroineen
+sekä lyhyt selstu siitä, mitä kuvassa taphtuu
+kuvan ottajalla on ltava kuvattavan henkilön suostumus kuvan julkaisemiseen
+
+valokuvassa pitää näkyä ihminen tai ihmisiä kuntoilemassa lomilemassa  työskentelmässä
+pääpalkinnon voi saada jälkeen kesän aikana joka viikko
+kuvaaja saa paperikuvat takaisin jos hän toimittaa lehteen
+palautusosoitteen maksetun palsutuskuoren osoitteinen
+yhteystiedot ja puhelinnumeron
+kuvan saa julkaista lehdessä jos kuvan henkillö on antanut luvan lehdelle
+kuvaaja on antanut luvan lehdelle kuvan henkiklö on antanut luvan kuvvajalle
+
+toivotaan ennen julkaisemattomia kuvia ihmisstä työn touhussa suomalaisella maaseudulla
+tekeminen voi olla yhtö hyvin elannon eteen tehtävää työtä kuin harrastustakin
+pääasia että kuvassa näkyy kunnon meininki
+kilpailu kestää syyskuun loppuunasti
+juhlavuoden kuvakilpailun voittanen kuvan ottaja palkitaan 500 euron arvoisella valokuvausliikkeen
+lahjakortilla
+kuvia kannattaa lähettää pitkin kessää sillä jokaisen kilpailuviikon paras kuva
+palkitaan kirjapalkinnolla
+kilpailukuvia voi lähettää aähköisesti jpg muodoss sähköpostiosoitteeeseen
+phote@maaseuduntulevaisuus.fi
+kuvatiedoston koko saa olla korkeintaa 5 mt
+paperikuvat postitetaan lehden toimitukseen
+kumpaankin tunnut tekemisen meininki
+mikäli haluat paperikuvat takaisin liitä mukaan postimerkillä ja paöaurusosoitteella
+varustettu kirjekuori
+muista liittä mukaan myös yhteystitosi puhelinnumeroineen sekö lyhyt selostus siiitöä
+mitä kuvassa tapahtuu
+kuvan ottajalla on oltava kuvattavan henkilön suostumus kuvan julkaisemiseen
+
+ihminen tai ihmisiä kuntoilemassa lomailmass työskentelmässa
+
+toivotaan ennen julkaisemattomia kuvia ihmisistiä työn touhusssa suomalaisella maaseudulla
+tekeminen voi olla yhtä hyvin elannon eteen tehtävää työtä kuin harrastustakin
+pääasia että kuvassa näkyy kunnon meininki
+kilpailu kestää syyskuun loppuun asti
+juhlavuoden kuvakilpailun voittanen kuvan ottaja palkitaan 500 euron arvoisella valoskuvausliikkeen
+lahjakorilla
+kuvia kannatta lähettää pitkion kesää sillä jokaisen kilpailuviikon paras kuva julkaistaan perjantain
+lehdessä ja sen ottaja palkitaan kirjapalkinnolla
+sillä jokaisen kilpailuviikon paras kuva julkaistaa perjantain lehdessä ja sen ottaja palkitaan kirjapalkinnolla
+palkitaan kirjapalkinnolla palkitaan kirjapalkinnolla kirjapalkinnolla
+
+kluvaaja saa paperikuvat takaisin jos hän toimitttaa lehteen
+poalautusosoitteen maksetun palautuskuoren osoitteineen
+yhtystiedot ja puhelinnumeron
+kuvaaja saa paperikuvat palautusosoitteen maksetun palautuskuoren osoitteineen
+yhteystiedot ja puhelinnumeron
+
+mikäli haluat paperikuvat takaisin liitä mukaan postimerkillä ja palautusosoitteella varusttetuur kirjekuori
+muista liittää mukaan myös yhteystietosi puheilinnumeroineen sekä lyhyt selostus siitä
+mitä kuvassa liitä mukaan varustettu
+kuvan saa julkaista lehdessä jos kuvan henkilö on antanut luvan lehdelle
+kuvaaja on antanut luvan lehdelle kuvan henkilö on antanut luvan kuvaajalle
+kuvan saa julkaista lehdessä kuvan henkilö on antanut luvan lehdelle
+kuvaaja on antanut luvan lehdelle kuvan henkilö on antanut luvan kuvaajalle
+kuvan henkilä on antanut luvan lehdelle kuvaaja on antanut luvan lehdelle
+kuvan henkilö on antnut luvan kuvaajalle
+kuvan henkilöä on antanut luvan lehdelle
+kuvaaja on antanut luvan lehdelle
+kuvan henkilö on antnut luvan kuvaajalle
+
+kuvan ottajalla on lotava kuvattavan henkilön suostumus kuvan julkaisemiseen
+kuvan ottajalla on lotava kuvattavan henkilön suostumus kuvan julkaisemiseen
+kuvan ottajalla on oltava kuvattavan henkilön suostumus kuvan julkaisemiseen
+kuvan ottajalla on oltava kuvattavan suostumus kunvan julkaisemiseen
+
+valokuvakilpailu
+description tekemisen meininki valokuvakilpailuun toivotaan ennen
+julkaisemattomia kuvia ihmisistä työn touhussa suomalaisella maaseudulla
+toivotaan ennen julkaisemattomia kuvia ihmisistä työn touhussa
+suomalaisella maaseudulla
+tekeminen voi olla yhtä hyvin elannon eteen tehtävä työtä
+kuin harrastustakin
+tekeminen voi olla yhtä hyvin elannon eteen tehtävä työtä
+tekeminen voi olla yhtä hyvin elannon eteen tehtävää työtä
+kuin harrastustakin
+pääasia että kuvassa näkyy kunnon meininki
+pääasia että kuvassa näkyyu kunnon meininki
+kilpailu kestää syyskuun loppuun asti
+kilpailu kestää syyskuun lopppuun asti
+kilpailu kestää syyskuun loppuun asti
+juhlavuoden kuvakilpailun voitttaneen kuvan ottaja
+palkitaan 500 euron arvoisella valokuvausliikkeen lahjakortilla
+juhlavuoden kuvakilpailun voittanen kuvan ottaja palkitaan 500 euron arvoisella
+valokuvausliikkeen lahja kortilla
+juhlavuoden kuvakilpailun voittaneen kuvan ottaja palkitaan 500 euron arvoisella
+valokuvausliikkeen lahjakorilla
+kuvia kannattaa lähettää pitkin kessä
+sillä jokaisen kilpailuviikon paras kuva julkaistaan
+kuvia kannatta lähettä pitkin kesää
+sillä jokaisen kilpailuviinkon
+sillä jokaisen kilpailuviikon paras kuva julkaistaaan perjantain lehdessä
+ja sen ottaja palkitaan kirjapalkinnolla
+palkitaan kirjapalkinnolla
+palkitaan kirjapalkinnolla
+sen ottaja palkitaan kirjapalkinnolla
+sen ottaja palkitaan kirja palkinnolla
+kilpailukuvia voi lähettä sähköisesti jpg-muodossa sähköpostiosoitteeseen
+photo@maaseuduntulevaissus.fi
+kuvatiedoston koko saa olla korkeintaan 5 mt
+paperikuvat postitetaan lehden toimitukseen
+paperkuvat postitetaan lehden toimitukseen
+postitetaan lehden toimitukseen
+lehden toimitukseen
+kumpaankin tunnus tekmisen meininki
+kuumpaankin tunnus tekemisen meininki
+mikäli haluat paperikuvat takaisin
+mikäli haluat paperikuvat takaisin
+liitä mukaan postimerkillä
+littä mukaan postimerkillä
+ja palautusosoitteella varustettu kirjekuori
+muista liittää mukaan myös yhteystietosi puhelinnumerooinen
+seklä lyhyt selostus siitä mitä kuvassa selostus
+tapahtuu kuvan ottajalla on oltava kuvattavan henkilön
+suostumus kuvan julkaisemiseen
+kuvan ottajalla on oltava kuvattavan henkilön suostumus kuvan julkaisemiseen
+kuvan ottajalla on oltava kuvattavan henkilön suostumus kuvan julkaisemiseen
+kuvan ottajalla on oltava kuvattavan henkilön suostumus kuvan julkaisemiseen
+kuvan ottajalla on oltava kuvattavan henkilön suostumus kuvan julksiasemiseen
+
+valokuvassa pitää näkyä ihminen tai ihmisiä työskentelmässä
+pääpalkinnon voi saada jälkeen
+kuvaaja saa paperikuvat takaisin jos hän toimittaa lehteen
+maksetun palautuskuoren osoitteineen
+kuvan saa julkaista lehdessä jos kuvan henkilä on antanut luvan kuvaajalle
+kuvan henkilä on antanut luvan kuvaajalle
+kuvan saa julkaista lehdessä jos kuvan henkilä on antanut luvan kuvaajalle
+kuvan henkilä on antanut luvan kuvaajalle
+kuvan henkilö on antanut luvan kuvaajalle
+kuvan henkilö on antnut luvan kuvaajalle
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# end
