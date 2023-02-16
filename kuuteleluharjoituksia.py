0bbf7b2a8895d08eec10091b9c570d3e69c6ae03
Stella <stellavir11@gmail.com>
2023-02-16 12:41:56 +0200

    Add files via upload

---
+++ kuuteleluharjoituksia.py
@@ -0,0 +1,18 @@
+d pitkäjänteinen 'avulias'
+-huolellinen-careful
+m määrätietioinen-determined kunnianhimoinen-ambitious
+-päättäväinen -saavuttakse
+p oma-aloitteinen palveluhenkinen
+
+m vastuuntuntoinen-responsible joustava neuvottelutaitoinen
+-vaikeavammaisia
+b huolellinen oikeudenmukainen
+-oppisopimuksella -koulunkäyntiavustaja-attendence+apua -kohta kaikien
+j kärsivällinen stressinsietokykyinen
+-it tukihenkilö
+
+oleellisimmat
+sellutehtaasta
+mikäli provided
+alkukirjainta
+rehtori principal
